1c619e8f064cb11a59e5e0d49f53d2cec426f042
wangjiawei <cswangjiawei@163.com>
2023-12-14 07:51:40 +0800

    fix: thread_safe_queue bug

--- include/thread_safe_queue.h
+++ include/thread_safe_queue.h
@@ -14,10 +14,20 @@ private:
     std::condition_variable data_cond;
     
 public:
-    ThreadSafeQueue(/* args */);
-    ~ThreadSafeQueue();
-    void push(T new_value);
-    void wait_and_pop(T& value);
+ ThreadSafeQueue(/* args */){}
+ ~ThreadSafeQueue(){}
+ void push(T new_value) {
+    std::lock_guard<std::mutex> lk(mut);
+    data_queue.push(new_value);
+    data_cond.notify_one();
+ }
+
+ void wait_and_pop(T& value) {
+    std::unique_lock<std::mutex> lk(mut);
+    data_cond.wait(lk, [this]{return !data_queue.empty();});
+    value = std::move(data_queue.front());
+    data_queue.pop();
+ }
 };
 
 
